4e03090d1fa35f8485da183f7c5d63efde3b8ba7
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-05-07 16:40:29 -0300

    Adicionando tabelas de Genero e GeneroFilme

--- prisma/prismaClient.js
+++ prisma/prismaClient.js
@@ -2,4 +2,20 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+async function seedGeneros() {
+  await prisma.genero.createMany({
+    data: [
+      { descricao: "Ação" },
+      { descricao: "Comédia" },
+      { descricao: "Terror" },
+      { descricao: "Drama" },
+      { descricao: "Ficção Científica" }
+    ],
+  });
+  console.log("Gêneros adicionados com sucesso!");
+}
+
+// Chame a função apenas se necessário
+//seedGeneros();
+
 module.exports = prisma;
