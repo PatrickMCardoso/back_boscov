b9be445d87d0e6deaafee491b568c0e738e8831c
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-06-02 17:06:11 -0300

    Arrumando permissão nas rotas de Usuário

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -9,7 +9,7 @@ router.post('/usuario', createUser);
 // Rotas protegidas
 router.get('/usuarios', authenticateUser, authorizeAdmin, listUsers); // Apenas admin
 router.get('/usuario/:id', authenticateUser, authorizeAdminOrComum, getUserById); // Admin ou comum
-router.put('/usuario/:id', authenticateUser, authorizeAdmin, updateUser); // Apenas admin
+router.put('/usuario/:id', authenticateUser, authorizeAdminOrComum, updateUser); // Admin ou comum
 router.delete('/usuario/:id', authenticateUser, authorizeAdmin, deleteUser); // Apenas admin
 router.patch('/usuario/:id/reativar', authenticateUser, authorizeAdmin, reactivateUser); // Apenas admin
 
