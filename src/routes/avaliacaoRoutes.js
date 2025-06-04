9a39773d5bc087f0763e091b24ea8a3fbe6cbc8e
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-06-04 14:21:15 -0300

    Arrumando permissão das rotas

--- src/routes/avaliacaoRoutes.js
+++ src/routes/avaliacaoRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateUser, authorizeAdmin, authorizeAdminOrComum } = require('../middlewares/auth');
 const avaliacaoController = require('../controllers/avaliacaoController');
 
-router.post('/avaliacao', avaliacaoController.createAvaliacao);
-router.get('/avaliacoes', avaliacaoController.getAllAvaliacoes);
-router.get('/avaliacoes/usuario/:idUsuario', avaliacaoController.getAvaliacoesByUsuario);
+// Apenas usuários autenticados (admin ou comum) podem criar, editar e excluir avaliações
+router.post('/avaliacao', authenticateUser, authorizeAdminOrComum, avaliacaoController.createAvaliacao);
+router.put('/avaliacao/:idUsuario/:idFilme', authenticateUser, authorizeAdminOrComum, avaliacaoController.updateAvaliacao);
+router.delete('/avaliacao/:idUsuario/:idFilme', authenticateUser, authorizeAdminOrComum, avaliacaoController.deleteAvaliacao);
+
+// Apenas admin pode listar todas as avaliações
+router.get('/avaliacoes', authenticateUser, authorizeAdmin, avaliacaoController.getAllAvaliacoes);
+
+// Qualquer usuário autenticado pode ver suas próprias avaliações
+router.get('/avaliacoes/usuario/:idUsuario', authenticateUser, authorizeAdminOrComum, avaliacaoController.getAvaliacoesByUsuario);
+
+// Listar avaliações de um filme é público
 router.get('/avaliacoes/filme/:idFilme', avaliacaoController.getAvaliacoesByFilme);
-router.put('/avaliacao/:idUsuario/:idFilme', avaliacaoController.updateAvaliacao);
-router.delete('/avaliacao/:idUsuario/:idFilme', avaliacaoController.deleteAvaliacao);
 
 module.exports = router;
--- src/routes/filmesRoutes.js
+++ src/routes/filmesRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateUser, authorizeAdmin } = require('../middlewares/auth');
 const { createFilme, listFilmes, getFilmeById, updateFilme, deleteFilme, reactivateFilme, searchFilmes } = require('../controllers/filmesController');
 
-router.post('/filme', createFilme);
+// Rotas protegidas (apenas admin pode criar, editar, excluir, reativar)
+router.post('/filme', authenticateUser, authorizeAdmin, createFilme);
+router.put('/filme/:id', authenticateUser, authorizeAdmin, updateFilme);
+router.delete('/filme/:id', authenticateUser, authorizeAdmin, deleteFilme);
+router.patch('/filme/:id/reativar', authenticateUser, authorizeAdmin, reactivateFilme);
+
+// Rotas públicas (listar e buscar filmes)
 router.get('/filmes', listFilmes);
 router.get('/filme/:id', getFilmeById);
-router.put('/filme/:id', updateFilme);
-router.delete('/filme/:id', deleteFilme);
-router.patch('/filme/:id/reativar', reactivateFilme);
 router.get('/filmes/search', searchFilmes);
 
 module.exports = router;
